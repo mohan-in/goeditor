a86d8bb2158c68b2d426159c19c2953ca30442a4
gocode <mail2mohanram@gmail.com>
2015-08-30 21:46:51 +0530

    refactored ajax calls

--- static/ajax.js
+++ static/ajax.js
@@ -1,16 +1,23 @@
-var xhr = new XMLHttpRequest();
-
 function ajax(o) {
+    var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             if(xhr.status == 200){
-                o.success();
+                o.success(xhr.response);
             } else {
                 o.error();
             }
         }
     }
 
+    var data = new FormData();
+
+    for (var key in o.data) {
+        if (o.data.hasOwnProperty(key)) {
+            data.append(key, o.data[key]);
+        }
+    }
+
     xhr.open(o.method, o.url);
-    xhr.send(o.data);
+    xhr.send(data);
 }
